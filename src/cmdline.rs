53e4041bda8aece78fae4a9246791c625d22b4ce
NeterOster <neteroster@gmail.com>
2023-03-08 13:15:02 +0800

    Update cmdline.rs

--- src/cmdline.rs
+++ src/cmdline.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use std::path::Path;
 
 pub struct CmdChat {
@@ -11,9 +12,50 @@ pub enum CmdOperationType {
     QuitCmd,
 }
 
-impl<S> From<S> for CmdOperationType
+pub struct TryFromWrapper<T>(pub T);
+
+impl<T> From<T> for TryFromWrapper<T> {
+    fn from(value: T) -> Self {
+        Self(value)
+    }
+}
+
+impl<T> TryFromWrapper<T> {
+    pub fn extract(self) -> T {
+        self.0
+    }
+}
+
+#[derive(Debug)]
+pub struct CmdParseError;
+
+impl fmt::Display for CmdParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "cmd parse error.")
+    }
+}
+
+impl std::error::Error for CmdParseError {}
+
+impl<S> TryFrom<TryFromWrapper<S>> for CmdOperationType
 where
     S: Into<String>,
 {
-    
+    type Error = CmdParseError;
+    fn try_from(s: TryFromWrapper<S>) -> Result<Self, Self::Error> {
+        let s: String = s.extract().into();
+        let mut cmd = s.split(' ').into_iter();
+
+        match cmd.next().ok_or(CmdParseError)? {
+            "save" => Ok(Self::SaveContext(
+                cmd.next().ok_or(CmdParseError)?.to_string(),
+            )),
+            "read" => Ok(Self::ReadContext(
+                cmd.next().ok_or(CmdParseError)?.to_string(),
+            )),
+            "clear" => Ok(Self::ClearContext),
+            "quit" => Ok(Self::QuitCmd),
+            _ => Err(CmdParseError),
+        }
+    }
 }
